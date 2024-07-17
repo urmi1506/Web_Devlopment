d9018127efdf4cc9f2717a6f2c0d387baa49e25f
urmi1506 <urmilaawasarmal@gmail.com>
2024-07-17 21:39:12 +0530

    new code

---
+++ JS_Practice/HackerRank_JS_Practice/AreaRectangle.js
@@ -0,0 +1,9 @@
+function getArea(length, width) {
+    let area;
+
+    area=length*width;
+    console.log(area);
+    return area;
+}
+
+getArea(3,4.5);
---
+++ JS_Practice/HackerRank_JS_Practice/PerimeterRec.js
@@ -0,0 +1,7 @@
+function getPerimeter(length, width) {
+    let perimeter;
+      perimeter=2*(length+width);
+      console.log(perimeter);
+    return perimeter;
+}
+getPerimeter(4,4.5);
